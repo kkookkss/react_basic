6d70a7707942ee6c5189944fab980684898b100b
kkookkss <pre0919@gmail.com>
2022-10-24 06:47:50 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "엘리";
+  const list = ["우유", "딸기", "바나나", "요거트"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="orange">{`Hello ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+        {/* {list.map(function (item) {
+          return <li>{item}</li>;
+        })} */}
+      </ul>
+      <img
+        style={{ width: "300px", height: "200px" }}
+        src="https://images.unsplash.com/photo-1666440816033-36430909a03e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80"
+        alt="nature"
+      />
+    </>
   );
 }
 
